3f55fa6b7adfdca793462f62dd3437d865b30bce
Daniel Hazelbaker <daniel@blueboxmoon.com>
2016-12-01 12:20:49 -0800

    Initial import.

---
+++ LavaFromEntity.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Reflection;
+
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+using Rock.Web.UI.Controls;
+using Rock.Workflow;
+
+namespace com.shepherdchurch.WorkflowActions
+{
+    /// <summary>
+    /// Runs Lava and sets an attribute's value to the result.
+    /// </summary>
+    [ActionCategory( "Utility" )]
+    [Description( "Loads an entity object then passes it through Lava and sets an attribute's value to the result." )]
+    [Export( typeof( ActionComponent ) )]
+    [ExportMetadata( "ComponentName", "Lava From Entity" )]
+
+    [EntityTypeField( "Entity Type", "The type of entity to use when loading from the guid.", true, "", 0 )]
+    [WorkflowAttribute( "Entity Attribute", "The attribute that contains the GUID value to load the Entity from.", true, "", "", 1 )]
+    [WorkflowAttribute( "Attribute", "The attribute to store the result in.", true, "", "", 2 )]
+    [CodeEditorField( "Lava", "The <span class='tip tip-lava'></span> to run. The entity object will be available as 'Entity'.", CodeEditorMode.Lava, CodeEditorTheme.Rock, 300, true, "", "", 3, "Value" )]
+    public class LavaFromEntity : ActionComponent
+    {
+        /// <summary>
+        /// Executes the specified workflow.
+        /// </summary>
+        /// <param name="rockContext">The rock context.</param>
+        /// <param name="action">The action.</param>
+        /// <param name="entity">The entity.</param>
+        /// <param name="errorMessages">The error messages.</param>
+        /// <returns></returns>
+        public override bool Execute( RockContext rockContext, WorkflowAction action, Object entity, out List<string> errorMessages )
+        {
+            errorMessages = new List<string>();
+
+            //
+            // Get the GUID values for the Attribute to store the value into and the Attribute that
+            // contains the Entity GUID.
+            //
+            Guid attributeGuid = GetAttributeValue( action, "Attribute" ).AsGuid();
+            Guid entityAttributeGuid = GetAttributeValue( action, "EntityAttribute" ).AsGuid();
+            if ( !attributeGuid.IsEmpty() && !entityAttributeGuid.IsEmpty() )
+            {
+                //
+                // Now get the actual Attribute objects so we know what keys to use.
+                //
+                var attribute = AttributeCache.Read( attributeGuid, rockContext );
+                var entityAttribute = AttributeCache.Read( entityAttributeGuid, rockContext );
+                if ( attribute != null && entityAttribute != null )
+                {
+                    Guid entityGuid = Guid.Empty;
+
+                    //
+                    // Get the entity GUID value.
+                    //
+                    if ( entityAttribute.EntityTypeId == new Workflow().TypeId )
+                    {
+                        entityGuid = action.Activity.Workflow.GetAttributeValue( entityAttribute.Key ).AsGuid();
+                    }
+                    else if ( entityAttribute.EntityTypeId == new WorkflowActivity().TypeId )
+                    {
+                        entityGuid = action.Activity.GetAttributeValue( entityAttribute.Key ).AsGuid();
+                    }
+
+                    //
+                    // Find the Entity Type that we have been configured to use.
+                    //
+                    var entityTypeCache = EntityTypeCache.Read( GetAttributeValue( action, "EntityType" ).AsGuid() );
+                    if ( entityTypeCache != null )
+                    {
+                        string value = GetAttributeValue( action, "Value" );
+                        var mergeFields = GetMergeFields( action );
+
+                        //
+                        // Load the dynamic entity from the database and put it in the merge fields.
+                        //
+                        var dynamicEntity = GetEntityFromTypeByGuid( entityTypeCache.GetEntityType(), entityGuid );
+                        mergeFields.Add( "Entity", dynamicEntity );
+
+                        value = value.ResolveMergeFields( mergeFields );
+
+                        //
+                        // Store the result into the attribute.
+                        //
+                        if ( attribute.EntityTypeId == new Workflow().TypeId )
+                        {
+                            action.Activity.Workflow.SetAttributeValue( attribute.Key, value );
+                            action.AddLogEntry( string.Format( "Set '{0}' attribute to '{1}'.", attribute.Name, value ) );
+                        }
+                        else if ( attribute.EntityTypeId == new WorkflowActivity().TypeId )
+                        {
+                            action.Activity.SetAttributeValue( attribute.Key, value );
+                            action.AddLogEntry( string.Format( "Set '{0}' attribute to '{1}'.", attribute.Name, value ) );
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Load an Entity from the database given it's Type and unique GUID.
+        /// </summary>
+        /// <param name="entityType">The object type which inherits from IEntity.</param>
+        /// <param name="guid">The unique GUID of this entity that will be loaded.</param>
+        /// <returns>An instance of entityType or null if not found.</returns>
+        object GetEntityFromTypeByGuid( Type entityType, Guid guid )
+        {
+            IService service = GetServiceFromEntityType( entityType );
+
+            if ( service != null )
+            {
+                MethodInfo mi = service.GetType().GetMethod( "Get", BindingFlags.Instance | BindingFlags.Public, null, new Type[] { typeof( Guid ) }, null );
+
+                if ( mi != null )
+                {
+                    try
+                    {
+                        return mi.Invoke( service, new object[] { guid } );
+                    }
+                    catch
+                    {
+                        return null;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Find and return an IService subclass for the given Entity Type. This would be equivalent to returning
+        /// a PersonService instance when passed a Person entity type.
+        /// </summary>
+        /// <param name="entityType">The IEntity type that we need an IService object for.</param>
+        /// <returns>Instance of an IService subclass or null if not found.</returns>
+        protected IService GetServiceFromEntityType( Type entityType )
+        {
+            //
+            // Check all assemblies that are non-dynamic (they throw exceptions) that do not
+            // begin with 'System.' or 'Microsoft.' as those will never contain IServce
+            // class types.
+            //
+            foreach ( var assembly in AppDomain.CurrentDomain.GetAssemblies().Where( a => !a.IsDynamic && !a.FullName.StartsWith("System.") && !a.FullName.StartsWith("Microsoft.") ) )
+            {
+                var types = assembly.GetExportedTypes().Where( t => typeof( IService ).IsAssignableFrom( t ) );
+
+                //
+                // Loop through each public type that inherits from IService.
+                //
+                foreach ( var type in types )
+                {
+                    //
+                    // Check if this type is a generic of the entityType we are concerned with.
+                    //
+                    if (IsTypeGenericOf(type, entityType))
+                    {
+                        //
+                        // Find a constructor that takes a single DbContext subclass argument.
+                        //
+                        foreach ( var mi in type.GetConstructors() )
+                        {
+                            var parameters = mi.GetParameters();
+                            if ( parameters.Length == 1 && typeof( DbContext ).IsAssignableFrom( parameters[0].ParameterType ) )
+                            {
+                                //
+                                // Create an instance of the specific subclass using the default constructor and then
+                                // try to create an instance of the IService type.
+                                //
+                                DbContext context = (DbContext)Activator.CreateInstance( parameters[0].ParameterType, null );
+
+                                return ( IService )Activator.CreateInstance( type, new object[] { context } );
+                            }
+                        }
+
+                        return null;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Check if the type is a generic of the target type. For example, type&lt;targetType&gt;.
+        /// </summary>
+        /// <param name="type">The generic type to be checked.</param>
+        /// <param name="targetType">The type that is being wrapped by the generic type.</param>
+        /// <returns>true if type is a generic of the target type otherwise false.</returns>
+        bool IsTypeGenericOf( Type type, Type targetType )
+        {
+            if ( type.IsGenericType )
+            {
+                foreach ( Type arg in type.GetGenericArguments() )
+                {
+                    if ( arg == targetType )
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            //
+            // Recursively check the base type to see if it meets the criteria.
+            //
+            if ( type.BaseType != null )
+            {
+                return IsTypeGenericOf( type.BaseType, targetType );
+            }
+
+            return false;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,154 @@
+﻿## Lava From Entity
+
+This action takes an attribute that contains a GUID and loads the
+entity from the database and then allows you to format that Entity
+via Lava. You configure the Entity Type that is expected in the
+Settings for the action. Think of this as the `Attribute Set from
+Entity` action, except that you can tell it what Entity you want.
+
+##### Configuration
+
+* Entity Type - The `IEntity` type to load.
+* Entity Attribute - The `Attribute` that holds the GUID of the
+entity object to load.
+* Attribute - The `Attribute` to store the results into.
+* Lava - The lava text to parse.
+
+##### Example Usage
+
+Let's say we have an Attribute called *Member* that contains a GUID value for a
+GroupMember entity and we want to generate some text that says
+`Thank you Daniel for joining the Cat Lovers group.`. We
+would set the `Entity Type` to GroupMember; `Entity Attribute`
+to *Member*; `Attribute` to *Value* and then set our `Lava` to
+`Thank you {{ Entity.Person.FirstName }} for joining the {{ 
+Entity.Group.Name }} group.`
+
+## Loop Over String
+
+Think of this as a `for` loop in C or C#. This action takes a
+string value from an Attribute and splits it into components. Each
+component is then used to execute another activity in the workflow.
+
+##### Configuration
+
+* Source - The `Attribute` that has the string.
+* Seperator - The string to use when splitting the Source string
+(usually a comma).
+* Execute Each - The `Activity` to activate for each iteration.
+* Execute After - The `Activity` to activate at the end of all
+the iterations have executed.
+* Index Key - The key name of the `Attribute` that holds the
+current index in the loop in the spawned Activity. (optional)
+* Value Key - The key name of the `Attribute` that the element
+will be placed into in the spawned Activity. (optional)
+
+Order matters. Concurrent activities are executed in order from top
+to bottom. A currently executing activity will finish execution
+before another activity begins execution. This means if you want
+the loop to execute activity `B` for the loop iterations and then
+execute activity `C` at the end of the loop, make sure `C` comes
+after `B` in the activity order; otherwise you will find your "end
+of loop" activity executing before the others finish.
+
+> Note: While your activities will *probably* be executed in
+> iteration order, you should not depend on it. For example, if
+> you are using this to build a list of names, do not assume that
+> just because `Index` == 0 that this will be the first name.
+> Check the target attribute to see if it is empty instead.
+
+> Note: Any actions that follow this action in the same Activity
+> will be executed before your looped Activities run. For this
+> reason you need to use the `Execute After` activity for any
+> logic you want to run at the end of the loop.
+
+##### Example Usage
+
+When the Action fires it split the `Source` attribute by the
+contents of the `Seperator` attribute into a temporary array. For
+each component of this array it will execute the Activity defined
+by the `Execute Each` attribute. If the activity has an
+attributed that matches the value of the `Index Key` attribute
+then its value will be set to the integer number of the index of
+the element in the array. If the activity has an attribute that
+matches the value of the `Value Key` attribute then its value
+will be set to string value of the component from the array.
+
+Here is a simple example of a workflow with two Activities:
+
+* `Setup`
+  * Loop Over String
+* `Process`
+  * Attribute Set to Group Leader
+  * Assign Activity from Attribute Value
+  * Email Send
+
+This will send an e-mail to the leader of the small group identified
+by the GUID component of the `Source` during each iteration of
+the loop.
+
+Because Workflows execute a single activity and action at a time
+some more complex usages take a little more creativity. You cannot,
+for example, have the `Process` activity send a User Entry Form
+action request to each group leader and expect them to all receive
+an e-mail at the same time. The first group leader will receive an
+e-mail and once they have filled out the form and submitted it
+then execution of the next `Process` iteration begins.
+
+Basically, even though the term `Active` is used it is better to
+think of it as:
+* Many activities can be pending (active), but only one is
+being executed at at time and that activity must complete before
+executing the next pending (active) activity. Only one activity is
+truly *active* **and executing** at a time.
+
+In short, a single workflow is a synchronous execution context.
+
+>Note: Using this action in a workflow that is automatically
+>persisted should be avoided if possible. If you have a string
+>with 100 elements in it you will be spinning up 100 activities
+>in this workflow. If it is persisted to the database then that
+>means all 100 activities are also persisted to the database.
+>
+>Be nice to your server and don't persist looping workflows unless
+>you absolutely must.
+
+##### Asynchronous Usage
+
+A more complex usage can be accomplished with multiple workflows.
+For example, lets take the above example of needing to send a
+User Entry Form to each small group leader and convert it into
+something that will truly run asynchronously.
+
+* Workflow `Main`
+  * `Setup`
+    * Loop Over String
+  * `Process`
+    * Activate Workflow `Send Leader Email`
+* Workflow `Send Leader Email`
+  * `Main`
+    * Attribute Set to Group Leader
+    * Assign Activity from Attribute Value
+    * User Entry Form
+
+The above usage example performs a synchronous loop. All the
+actions will be completed for each iteration before the next
+iteration begins. However, because we are spinning off a new
+workflow, each of those workflows will begin to process as well
+and they will all run concurrently. This then becomes an
+asynchronous task and the `Main` workflow completes after
+initiation all of the required `Send Leader Email` workflows.
+
+You may notice, that in this case the `Main` workflow has no
+knowledge of then all the `Send Leader Email` workflows have
+finished. Therefore making use of the `Execute After` setting
+does not make much sense. You can still use it but it will almost
+definately be executed before the child workflows have finished.
+
+## Activate Activity With Attributes
+
+This provides the same functionality as the normal `Activate
+Activity` but provides the additional functionality of being
+able to map attribute values from the current activity to the one
+being activated. Think of this as passing arguments to the
+activity.
